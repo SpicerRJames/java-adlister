23b2f95534a8f91b90858e2a58379c6a499d83b1
James Spicer <spicer.r.james@gmail.com>
2020-04-29 15:45:54 -0500

    jdbc exercise

---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.util.List;
+
+public class MySQLAdsDao implements Ads {
+
+
+    private Connection connection;
+
+    public MySQLAdsDao(){
+
+    }
+
+
+    @Override
+    public List<Ad> all() {
+        return null;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        return null;
+    }
+}
